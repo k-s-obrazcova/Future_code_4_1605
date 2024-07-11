fc4d45f2e31c34eaf8203c9ed291a20e4756dbb3
ObraztsovaKsenia <ksynya_obr@mail.ru>
2024-07-11 17:48:31 +0300

    11.07.2024

--- shop/tests/tests.py
+++ shop/tests/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from shop.utils import *
 
+
 class CalculateMoneyDefTestCase(TestCase):
     def test_sum_price_count(self):
         result = sum_price_count(price=100, count=10)
@@ -16,3 +17,26 @@ class CalculateMoneyDefTestCase(TestCase):
         self.assertEqual(result, 2850)
 
 
+class CalculateMoneyClassTestCase(TestCase, CalculateMoney):
+    def test_sum_price_count_pass(self):
+        result = self.sum_price_count(price=400, count=19)
+        self.assertEqual(result, 7600)
+
+    def test_sum_price_count_with_discount(self):
+        result = self.sum_price_count(price=400, count=30, discount=10)
+        self.assertEqual(result, 10800)
+
+    def test_sum_price(self):
+        list_prices = [294, 2000, 100]
+        result = self.sum_price(prices=list_prices)
+        self.assertEqual(result, 2394)
+
+    def test_sum_price_with_discount(self):
+        list_prices = [200, 640, 720]
+        result = self.sum_price(prices=list_prices, discount=10)
+        self.assertEqual(result, 1404)
+
+    def test_sum_price_more_element(self):
+        list_price = [140, 25, 678, 70, 180, 21]
+        result = self.sum_price(prices=list_price)
+        self.assertEqual(result, 1114)
